e789b2c7f4dc95607e3dd0e2f840837a91b3f867
Soham Parsodkar <146697219+SohamParsodkar19@users.noreply.github.com>
2023-10-24 15:33:23 +0530

    Update KBC.py

--- KBC.py
+++ KBC.py
@@ -53,6 +53,7 @@ for i in range(0,len(questions)):
     
     question = questions[i]
     print(f"Question for Rs. {levels[i]}")
+    print(question[0])
     print(f"a. {question[1]}             b. {question[2]} ")
     print(f"c. {question[3]}             d. {question[4]} ")
     reply = int(input("Enter your answer (1-4):  or 0 to quit:"))
